7418683d31e89540aaab5c22484e884baa49be53
Nazar <tryuhann@gmail.com>
2025-08-16 14:33:19 +0300

    addd

---
+++ js/mobile.js
@@ -0,0 +1,9 @@
+(() => {
+  const toggleBtn = document.querySelector("[data-mobile-toggle]");
+  const mobileMenu = document.querySelector("[data-mobile]");
+
+  toggleBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+  });
+})();
