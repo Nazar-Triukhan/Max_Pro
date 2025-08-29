3dabfa79af9a18a97255e473faaf6aff9986f90a
Nazar <tryuhann@gmail.com>
2025-08-29 17:46:20 +0300

    add

--- js/scroll-x.js
+++ js/scroll-x.js
@@ -1,42 +1,42 @@
-const box = document.querySelector('.networks__box');
-const items = document.querySelector('.networks__items');
+// const box = document.querySelector('.networks__box');
+// const items = document.querySelector('.networks__items');
 
-// сохраняем ширину оригинального списка
-const originalWidth = items.scrollWidth;
+// // сохраняем ширину оригинального списка
+// const originalWidth = items.scrollWidth;
 
-// клонируем список ещё дважды
-items.innerHTML = items.innerHTML + items.innerHTML + items.innerHTML + items.innerHTML + items.innerHTML + items.innerHTML + items.innerHTML + items.innerHTML + items.innerHTML;
+// // клонируем список ещё дважды
+// items.innerHTML = items.innerHTML + items.innerHTML + items.innerHTML + items.innerHTML + items.innerHTML + items.innerHTML + items.innerHTML + items.innerHTML + items.innerHTML;
 
-// ставим скролл в середину (второй клон)
-box.scrollLeft = originalWidth;
+// // ставим скролл в середину (второй клон)
+// box.scrollLeft = originalWidth;
 
-// обработчик скролла
-box.addEventListener('scroll', () => {
-  const scrollLeft = box.scrollLeft;
+// // обработчик скролла
+// box.addEventListener('scroll', () => {
+//   const scrollLeft = box.scrollLeft;
 
-  // если ушли вправо — возвращаем в середину
-  if (scrollLeft >= originalWidth * 5) {
-    box.scrollLeft = scrollLeft - originalWidth;
-  }
+//   // если ушли вправо — возвращаем в середину
+//   if (scrollLeft >= originalWidth * 5) {
+//     box.scrollLeft = scrollLeft - originalWidth;
+//   }
 
-  // если ушли влево — возвращаем в середину
-  if (scrollLeft <= originalWidth / 5) {
-    box.scrollLeft = scrollLeft + originalWidth;
-  }
-});
-// const track = document.querySelector('.networks__items');
-// const speed = 1; // скорость прокрутки
-// let pos = 0;
+//   // если ушли влево — возвращаем в середину
+//   if (scrollLeft <= originalWidth / 5) {
+//     box.scrollLeft = scrollLeft + originalWidth;
+//   }
+// });
+const track = document.querySelector('.networks__items');
+const speed = 1; // скорость прокрутки
+let pos = 0;
 
-// // клонируем список
-// track.innerHTML += track.innerHTML + track.innerHTML;
-// const width = track.scrollWidth / 3;
+// клонируем список
+track.innerHTML += track.innerHTML + track.innerHTML;
+const width = track.scrollWidth / 3;
 
-// function animate() {
-//   pos -= speed; // движение влево
-//   if (pos <= -width) pos = 0;
-//   track.style.transform = `translateX(${pos}px)`;
-//   requestAnimationFrame(animate);
-// }
+function animate() {
+  pos -= speed; // движение влево
+  if (pos <= -width) pos = 0;
+  track.style.transform = `translateX(${pos}px)`;
+  requestAnimationFrame(animate);
+}
 
-// animate();
+animate();
