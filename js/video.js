491203ad7d8fe27aae9a4901ba3ebc31dce2f716
Nazar <tryuhann@gmail.com>
2025-09-01 12:04:05 +0300

    фвв

--- js/video.js
+++ js/video.js
@@ -1,9 +1,14 @@
-const videos = document.querySelectorAll('.services__video');
+document.addEventListener('DOMContentLoaded', () => {
+  const videos = document.querySelectorAll('.services__video');
 
-videos.forEach(video => {
-  video.addEventListener('loadeddata', () => {
-    video.currentTime = 0;  // перейти на перший кадр
-    video.pause();           // зупинити відео, щоб не автоплей
+  videos.forEach(video => {
+    // Коли відео завантажило дані
+    video.addEventListener('loadeddata', () => {
+      video.currentTime = 0;  // перший кадр
+      video.pause();           // зупинка відео, щоб не автоплей
+    });
+
+    // Для деяких браузерів можна спробувати force load
+    video.load();
   });
 });
-  
